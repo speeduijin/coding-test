889f9fd3931998b5cf0b30509cdf74edb5870bd7
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-08 15:17:01 +0900

    b10871_O

--- b10871/app.js
+++ b10871/app.js
@@ -3,6 +3,17 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 const input = fs.readFileSync(filePath).toString().split("\n");
 
-solution();
+const [inputN, inputX] = input[0].split(" ").map((item) => +item);
+const arrayA = input[1].split(" ").map((item) => +item);
 
-function solution() {}
+solution(inputN, inputX, arrayA);
+
+function solution(N, X, A) {
+  const answer = [];
+  for (let i = 0; i < N; ++i) {
+    if (arrayA[i] < X) {
+      answer.push(A[i]);
+    }
+  }
+  console.log(answer.join(" "));
+}
