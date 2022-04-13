d8f8ad5b481b64e8cced2a649bc76907a3dd6db2
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-13 16:14:21 +0900

    b1065_O

--- b1065/app.js
+++ b1065/app.js
@@ -5,4 +5,32 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function h(X) {
+  const str = String(X);
+  let ans = true;
+  if (str.length < 3) {
+    return true;
+  } else {
+    for (let i = 2; i < str.length; i++) {
+      const diff = +str[1] - +str[0];
+      let beforeNum = +str[1];
+
+      //   if (+str[i] - +str[i - 1] !== diff) {
+      if (+str[i] - beforeNum !== diff) {
+        return false;
+      }
+      beforeNum = +str[i];
+    }
+    return true;
+  }
+}
+
+function solution(N) {
+  let cnt = 0;
+  for (let i = 1; i <= N; i++) {
+    if (h(i)) {
+      cnt++;
+    }
+  }
+  console.log(cnt);
+}
