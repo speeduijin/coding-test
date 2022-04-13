9f6fd0fa1ed5d6e388958459618c5923e72f9615
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-13 17:16:35 +0900

    b1712_O

--- b1712/app.js
+++ b1712/app.js
@@ -1,8 +1,19 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
-const input = fs.readFileSync(filePath).toString();
+let input = fs.readFileSync(filePath).toString();
+
+input = input.split(" ").map((item) => +item);
 
 solution(input[0], input[1], input[2]);
 
-function solution(A, B, C) {}
+function solution(A, B, C) {
+  let result = 0;
+  const diff = C - B;
+  if (diff <= 0) {
+    result = -1;
+  } else {
+    result = Math.floor(A / diff) + 1;
+  }
+  console.log(result);
+}
