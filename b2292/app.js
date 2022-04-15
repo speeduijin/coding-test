b02ed8ab91ab6deb133bf5e94086d4c521ecd1cf
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-15 17:18:16 +0900

    b2292_O

--- b2292/app.js
+++ b2292/app.js
@@ -5,4 +5,13 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function solution(N) {
+  let cnt = 1;
+  N = N - 1;
+
+  while (N > 0) {
+    N -= 6 * cnt;
+    cnt++;
+  }
+  console.log(cnt);
+}
