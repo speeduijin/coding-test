f1c67ba07306270f1ce2216847a3698bf97d132f
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-16 15:27:09 +0900

    b1193_O

--- b1193/app.js
+++ b1193/app.js
@@ -5,4 +5,20 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(X) {}
+function solution(X) {
+  let cnt = 0;
+
+  while (X > 0) {
+    cnt++;
+    X -= cnt;
+  }
+
+  const B = cnt + X;
+  const A = 1 - X;
+
+  if (cnt % 2 === 0) {
+    console.log(`${B}/${A}`);
+  } else {
+    console.log(`${A}/${B}`);
+  }
+}
