36810aa7d5cb7a0b5a5886fd577098f4f5fe1668
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-19 19:10:01 +0900

    b2839_O

--- b2839/app.js
+++ b2839/app.js
@@ -5,4 +5,29 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function solution(N) {
+  let cnt = 0;
+  const fiveW = 5;
+  const threeW = 3;
+
+  if (N % fiveW !== 0) {
+    while (N > 0) {
+      if (N > 2) {
+        if (N % fiveW === 0) {
+          N / fiveW;
+          cnt += N / fiveW;
+          break;
+        } else {
+          N -= threeW;
+          cnt++;
+        }
+      } else {
+        cnt = -1;
+        break;
+      }
+    }
+  } else {
+    cnt = N / fiveW;
+  }
+  console.log(cnt);
+}
