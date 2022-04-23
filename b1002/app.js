5ac34096e8b5f41e8c4b4f665430ef242a07e237
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-23 13:38:49 +0900

    b1002_O

--- b1002/app.js
+++ b1002/app.js
@@ -6,16 +6,30 @@ const input = fs.readFileSync(filePath).toString().split("\n");
 const inputArr = [];
 for (let i = 1; i <= +input[0]; i++) {
   const el = input[i].split(" ").map((item) => +item);
-  inputArr.push({
-    x1: el[0],
-    y1: el[1],
-    r1: el[2],
-    x2: el[3],
-    y2: el[4],
-    r2: el[5],
-  });
+  inputArr.push(el);
 }
 
 solution(+input[0], inputArr);
 
-function solution(T, testArr) {}
+function solution(T, testArr) {
+  const coordCnt = (el) => {
+    const [x1, y1, r1, x2, y2, r2] = el;
+    const pointDiff = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    const rSum = r1 + r2;
+    const rDiff = Math.abs(r2 - r1);
+
+    if (x1 === x2 && y1 === y2 && r1 === r2) {
+      return -1;
+    } else if (rSum < pointDiff || rDiff > pointDiff) {
+      return 0;
+    } else if (rSum === pointDiff || rDiff === pointDiff) {
+      return 1;
+    } else if (rSum > pointDiff || rDiff < pointDiff) {
+      return 2;
+    }
+  };
+
+  for (let i = 0; i < T; i++) {
+    console.log(coordCnt(testArr[i]));
+  }
+}
