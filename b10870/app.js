087574386df71d771c6897949da1c1bb0e490e26
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-28 16:18:42 +0900

    b10870_O

--- b10870/app.js
+++ b10870/app.js
@@ -5,4 +5,14 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function fbn(el) {
+  if (el === 0 || el === 1) {
+    return el;
+  } else {
+    return fbn(el - 1) + fbn(el - 2);
+  }
+}
+
+function solution(N) {
+  console.log(fbn(N));
+}
