6f4aa36f29dca857203b578b7f73544a1aad88ca
Jeong Eui-jin <speeduijin91@gmail.com>
2022-04-30 15:50:12 +0900

    b2447_O

--- b2447/app.js
+++ b2447/app.js
@@ -5,4 +5,27 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function solution(N) {
+  const ansArr = [];
+
+  const star = (i, j, n) => {
+    if (i % 3 === 1 && j % 3 === 1) {
+      ansArr.push(" ");
+    } else {
+      if (n === 1) {
+        ansArr.push("*");
+      } else {
+        star(parseInt(i / 3), parseInt(j / 3), parseInt(n / 3));
+      }
+    }
+  };
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      star(i, j, N);
+    }
+    ansArr.push("\n");
+  }
+
+  console.log(ansArr.join(""));
+}
