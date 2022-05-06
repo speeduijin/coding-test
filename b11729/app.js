e0b0acfb08232746de410f7b013b2cf0963aa4bc
Jeong Euijin <speeduijin91@gmail.com>
2022-05-06 18:59:24 +0900

    b11729_O

--- b11729/app.js
+++ b11729/app.js
@@ -5,4 +5,22 @@ const input = fs.readFileSync(filePath).toString();
 
 solution(+input);
 
-function solution(N) {}
+function solution(N) {
+  let count = 0;
+  const orderArray = [];
+
+  const orderOfHanoi = (n, from, other, to) => {
+    if (n === 0) {
+      return;
+    }
+    orderOfHanoi(n - 1, from, to, other);
+    count++;
+    orderArray.push([from, to]);
+    orderOfHanoi(n - 1, other, from, to);
+    console.log(orderArray);
+  };
+
+  orderOfHanoi(N, 1, 2, 3);
+  console.log(count);
+  console.log(orderArray.map((items) => items.join(" ")).join("\n"));
+}
