f28940f648968d86f3e9b9816b33dd0eac0c6fbe
Jeong Euijin <speeduijin91@gmail.com>
2022-05-30 15:00:22 +0900

    b2750_O

---
+++ b2750/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [inputN, ...inputArr] = input;
+
+solution(inputN, inputArr);
+
+function solution(N, array) {
+  array.sort((a, b) => a - b).map((item) => console.log(item));
+}
