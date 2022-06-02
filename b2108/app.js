da4b725b03c38cc3f1589739fe641214afaf0bf5
Jeong Euijin <speeduijin91@gmail.com>
2022-06-02 15:13:44 +0900

    b2108_X

---
+++ b2108/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [inputN, ...inputArr] = input;
+
+solution(inputN, inputArr);
+
+function solution(N, array) {}
