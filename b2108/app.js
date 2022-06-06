6be7433b6a64a32848dd7ebc02672caf1ac9654c
Jeong Euijin <speeduijin91@gmail.com>
2022-06-07 08:54:51 +0900

    b2108_O

--- b2108/app.js
+++ b2108/app.js
@@ -12,4 +12,43 @@ const [inputN, ...inputArr] = input;
 
 solution(inputN, inputArr);
 
-function solution(N, array) {}
+function solution(N, array) {
+  array.sort((a, b) => a - b);
+
+  let avg = Math.round(array.reduce((acc, cur) => acc + cur) / N);
+  if (avg === -0) {
+    avg = 0;
+  }
+
+  const mid = array[Math.floor(N / 2)];
+
+  const itemCnt = {};
+  itemCnt[array[0]] = 1;
+  for (let i = 1; i < N; i++) {
+    if (itemCnt[array[i]]) {
+      itemCnt[array[i]] += 1;
+    } else {
+      itemCnt[array[i]] = 1;
+    }
+  }
+
+  const maxCnt = Math.max(...Object.values(itemCnt));
+  const maxCntArray = Object.keys(itemCnt)
+    .filter((key) => {
+      if (itemCnt[key] === maxCnt) {
+        return key;
+      }
+    })
+    .map((item) => +item);
+
+  let maxCntIdx = 0;
+  if (maxCntArray.length > 1) {
+    maxCntArray.sort((a, b) => a - b);
+    maxCntIdx = maxCntArray[1];
+  } else {
+    maxCntIdx = maxCntArray[0];
+  }
+
+  const diff = array[N - 1] - array[0];
+  console.log(`${avg}\n${mid}\n${maxCntIdx}\n${diff}`);
+}
