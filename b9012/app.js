45a32b42b0f049ed10a35c332852404ec031bc7f
Jeong Euijin <speeduijin91@gmail.com>
2022-06-17 07:04:23 +0900

    b9012_O

--- b9012/app.js
+++ b9012/app.js
@@ -6,6 +6,34 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [, ...tempArr] = input;
 const inputArr = tempArr.map((item) => item.trim());
 
-solution();
+solution(inputArr);
 
-function solution() {}
+function solution(array) {
+  let resultArr = [];
+
+  array.forEach((el) => {
+    const stackArr = [];
+    let testNum = 0;
+
+    for (let i = 0; i < el.length; i++) {
+      stackArr.push(el[i]);
+      if (stackArr[stackArr.length - 1] === "(") {
+        testNum += 1;
+      } else {
+        testNum -= 1;
+      }
+
+      if (testNum === -1) {
+        resultArr.push("NO");
+        return;
+      }
+    }
+    if (testNum === 0) {
+      resultArr.push("YES");
+    } else {
+      resultArr.push("NO");
+    }
+  });
+
+  console.log(resultArr.join("\n"));
+}
