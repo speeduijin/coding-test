8cf48a07e47beab46ae2cd9576ffb3e48b5aac97
Jeong Euijin <speeduijin91@gmail.com>
2022-06-20 17:25:41 +0900

    b1874_O

--- b1874/app.js
+++ b1874/app.js
@@ -12,4 +12,23 @@ const [inputN, ...inputArr] = input;
 
 solution(inputN, inputArr);
 
-function solution(N, array) {}
+function solution(N, array) {
+  let result = "";
+  const stackArr = [];
+  const intArr = Array(N)
+    .fill(0)
+    .map((el, idx) => idx + 1);
+
+  for (let i = 0; i < N; i++) {
+    while (array[i] !== stackArr[stackArr.length - 1] && intArr[0] > 0) {
+      stackArr.push(intArr.shift());
+      result += "+\n";
+    }
+    if (array[i] === stackArr[stackArr.length - 1]) {
+      stackArr.pop(stackArr[stackArr.length - 1]);
+      result += "-\n";
+    }
+  }
+
+  stackArr.length !== 0 ? console.log("NO") : console.log(result);
+}
